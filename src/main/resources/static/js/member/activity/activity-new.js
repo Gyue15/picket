f6a4d2f2b40c63cbdd883cc3eaf3802c796423f8
gyue15 <786330297@qq.com>
2018-06-10 17:58:28 +0800

    首页

--- src/main/resources/static/js/member/activity/activity-new.js
+++ src/main/resources/static/js/member/activity/activity-new.js
@@ -1,10 +1,17 @@
-layui.use('carousel', function(){
-    let carousel = layui.carousel;
-    //建造实例
-    carousel.render({
-        elem: '#activity-gallery'
-        ,width: '80%' //设置容器宽度
-        ,arrow: 'always' //始终显示箭头
-        //,anim: 'updown' //切换动画方式
+$(function () {
+    let high = document.body.clientWidth * 0.8 / 3;
+    layui.use('carousel', function(){
+        let carousel = layui.carousel;
+        //建造实例
+        carousel.render({
+            elem: '#activity-gallery'
+            ,width: '80%' //设置容器宽度
+            ,arrow: 'always' //始终显示箭头
+            ,height: high
+            //,anim: 'updown' //切换动画方式
+        });
+    });
+    layui.use('element', function(){
+        // let element = layui.element;
     });
 });
