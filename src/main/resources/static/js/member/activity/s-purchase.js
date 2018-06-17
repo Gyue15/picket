fa958b1c2c0ddb2eb5addec0ae4ce3adbb7a7148
fraliphsoft <151250054@smail.nju.edu.cn>
2018-06-17 20:28:46 +0800

    Now you can select region by click the gragh other than click the selector

--- src/main/resources/static/js/member/activity/s-purchase.js
+++ src/main/resources/static/js/member/activity/s-purchase.js
@@ -9,9 +9,25 @@ $(function () {
     }).done(function (data) {
         let areaGraph = data.data;
         sCanvas.loadFromJSON(areaGraph, canvas.renderAll.bind(canvas), function (o, object) {
-            object.set({selectable: false, hoverCursor: 'default'});
+            object.set({
+            	selectable: true,
+            	lockMovementX: true,
+            	lockMovementY: true,
+            	hasControls: false,
+            	hasRotatingPoint: false,
+            	selectionBackgroundColor: 'rgba(150,150,150,0.5)',
+            	hoverCursor: 'default'
+            });
         });
-        sCanvas.selection = false;
+        $(sCanvas.lowerCanvasEl).next().on("click", function() {
+        	var obj = sCanvas.getActiveObject();
+        	var textContent = obj.areaName;
+        	$("#area-type dd").each(function(index, ele) {
+        		if (ele.innerHTML == textContent) {
+        			ele.click();
+        		}
+        	});
+        });
     }).fail(function (e) {
         alertWindow(e.responseText);
     })
