7662767ae001ddb988bed9920c301cf83f810531
fraliphsoft <151250054@smail.nju.edu.cn>
2018-06-30 10:04:27 +0800

    update the style of orders 1 hour ahead of time, so I can go back to my bed now.

--- src/main/resources/static/js/member/statistic/order.js
+++ src/main/resources/static/js/member/statistic/order.js
@@ -22,12 +22,18 @@ function updateList(domElement, state) {
 function displayList(domElement, data) {
 	var formatData = new Array();
 	for (var i=0; i<data.length; i++) {
+		var seatsAndPriceList = new Array();
+		for (var j=0; j<`${data[i].seatNameList.length}`; j++) {
+			seatsAndPriceList.push(`${data[i].seatNameList[j]}` + "(单价" + `${data[i].seatPriceList[j]}` + ")");
+		}
 		formatData.push({
 			'id' : `${data[i].orderId}`,
 			'pic': `<img src="/showpic/${data[i].activityId}.jpg" height="80" width="60"/>`,
 			'aname' : `${data[i].activityName}`,
 			'vname' : `${data[i].venueName}`,
-			'date' : `${data[i].placeDateString} 下单`
+			'date' : `${data[i].placeDateString}`,
+			'seats' : seatsAndPriceList,
+			'totalPrice' : `总额￥${data[i].orderValue}`,
 		});
 	}
 	$(domElement).bootstrapTable({
@@ -48,14 +54,39 @@ function displayList(domElement, data) {
     	}, {
     		field : 'vname'
     	}, {
-    		field : 'date'
+    		field : 'date',
+    		visible: false
+    	}, {
+    		field: 'seats',
+    		formatter : function formatter(value, row, index) {
+    			var htmlstr = "";
+    			if (value.length <=3 ){
+    				for (var i=0; i<value.length; i++) {
+    					htmlstr += "<div>" + value[i] + "</div>";
+    				}
+    			} else {
+    				for (var i=0; i<value.length; i+=2) {
+    					htmlstr += "<div>" + value[i] + "&emsp;" + ((i+1>=value.length)?"":value[i+1]) + "</div>";
+    				}
+    			}
+    			return htmlstr;
+    		}
+    	}, {
+    		field : 'totalPrice'
     	}, {
     		field : 'detail',
     		formatter : function formatter(value, row, index) {
-    			return "<a class='order-detail' style='padding:10px;' href='/member/order/detail?orderId=" + row.id + "'>查看详情</a>";
+    			return "<a class='order-detail' href='/member/order/detail?orderId=" + row.id + "'>查看详情&gt;&gt;</a>";
     		}
     	}],
-    	data : formatData,
+    	data : formatData
     });
     $("#tab-content .active");
+    $(domElement).find("tbody tr").wrap("<div class='panel panel-default'></div>");
+    $(domElement).find("tbody tr").before("<div class='panel-heading'></div>");
+    $(domElement).find("tbody .panel .panel-heading").each(function(index, element) {
+    	$(element).append("<div class='dateTag'>" + $(domElement).bootstrapTable("getData")[index].date + "</div>");
+    	$(element).append("<div class='idTag'>订单号:" + $(domElement).bootstrapTable("getData")[index].id + "</div>");
+    });
+    
 }
